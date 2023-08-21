2ebe7ac7fdd1d1c7627f11cd4aa3e0f5c59ee915
rinoa349 <chan2s@icloud.com>
2023-08-21 17:41:33 +0900

    Update image_slider.js

--- app/javascript/image_slider.js
+++ app/javascript/image_slider.js
@@ -1,11 +1,5 @@
 const swiper = new Swiper(".swiper", {
-  slidesPerView: 1,
-  breakpoints: {
-    // 768px以上の場合
-    768: {
-      slidesPerView: 3
-    }
-  },
+  slidesPerView: 3　/* この行を追加 */,
   // ページネーションが必要なら追加
   pagination: {
     el: ".swiper-pagination"
